b92650a3a2578716febf28242356e93ec4e6fdf5
Yahir Nieto <94183916+YANA021@users.noreply.github.com>
2025-09-04 12:53:34 -0600

    Agregando la descripcion y sus imagenes

--- archivo1.php
+++ archivo1.php
@@ -6,3 +6,10 @@ $CATS = [
   'amigos' => 'Amigos de Shrek',
   'secundarios' => 'Secundarios'
 ];
+$P = [
+  ['img'=>'imagenes/img1.jpg','nom'=>'Lord Farquaad','cat'=>'villanos','desc'=>'Tirano de Duloc.'],
+  ['img'=>'imagenes/img3.jpg','nom'=>'Shrek','cat'=>'principales','desc'=>'Ogro prota.'],
+  ['img'=>'imagenes/img2.jpg','nom'=>'hermanastra_mas_fea','cat'=>'secundario','desc'=>'Es un papucho, su cara parece tallada por los mismos angeles, el meme.'],
+  ['img'=>'imagenes/img4.jpg','nom'=>'Burro','cat'=>'amigos','desc'=>'Burro o donkey amigo del protagonista.'],
+  ['img'=>'imagenes/img5.jpg','nom'=>'jengibre','cat'=>'amigos','desc'=>'galleta de jengibre.'],
+];
